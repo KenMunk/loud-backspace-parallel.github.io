ede4d0f7c562c446e87a62f718d5104b6b395e13
Kenneth Munk <contact.me@kenmunk.com>
2022-11-06 14:09:23 -0800

    LB-78 -- Create basic layout page for roommate finder

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,8 +1,15 @@
+//Importing third party libraries
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-import StandardStyles from './pages/components/StandardStyles';
-import PageLayouts from './pages/components/PageLayouts';
+
+//import StandardStyles from './pages/components/StandardStyles';
+//import PageLayouts from './pages/components/PageLayouts';
+import PageLayouts from '@PageLayouts';
+
+//Importing project pages/tabs/content
+import RoommateFinder from './pages/RoommateFinder';
+
 
 const landscapeMenuBar = 250;
 
@@ -10,12 +17,19 @@ function contentSpaceWidth(){
 	
 	const screenWidth = useWindowDimensions().width;
 	
+	{
+		/*
 	if(screenWidth > 800){
-		return ((screenWidth - landscapeMenuBar));
+		return ((screenWidth - landscapeMenuBar)-15);
 	}
 	else{
 		return (screenWidth * 0.9);
 	}
+	
+	//*/
+	}
+	
+	return screenWidth-20
 	
 }
 
@@ -27,7 +41,7 @@ export default function App() {
 			<View style={ PageLayouts.topBar}>
 			</View>
 			<View style={ PageLayouts.contentSpace}>
-				{//*
+				{/*
 				(useWindowDimensions().width > 800) &&
 				<View style={[ 
 					PageLayouts.menuBar,
@@ -36,13 +50,14 @@ export default function App() {
 					<Text> Side Menu: </Text>
 					<Text>only available when screen is large enough </Text>
 				</View>
-				//*/}
+				//*/
 				}
 				<View style={[
 					PageLayouts.contentColumn, 
 					{width: contentSpaceWidth()}
 				]}>
-					<Text> Test </Text>
+					{<Text>Main Content</Text>}
+					<RoommateFinder/>
 				</View>
 			</View>
 		</View>
